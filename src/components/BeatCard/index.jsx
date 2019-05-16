8aef38bf6ab9716101cc9f302698602bed0e2d92
sharad-s <sharadshekar@gmail.com>
2019-05-16 12:31:27 -0500

    update card

--- src/components/BeatCard/index.jsx
+++ src/components/BeatCard/index.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-const borderRadius = "10px";
+const borderRadius = '10px';
 
 const StyledCard = styled.div`
 	width: 200px;
@@ -22,24 +22,24 @@ const StyledCard = styled.div`
 `;
 
 const StyledCardImage = styled.img`
-  width: inherit;
-  height: inherit;
-  object-fit: cover;
-  border-radius: ${borderRadius} ${borderRadius} 0 0;
+	width: inherit;
+	height: inherit;
+	object-fit: cover;
+	border-radius: ${borderRadius} ${borderRadius} 0 0;
 
-  // Flex Details 
+	// Flex Details
 `;
 
 const StyledCardDetails = styled.div`
 	height: 25%;
-  border-radius: 0 0 ${borderRadius} ${borderRadius};
-  // background-color: grey;
-  // border: 1px solid grey;
-  border-top: none;
+	border-radius: 0 0 ${borderRadius} ${borderRadius};
+	// background-color: grey;
+	// border: 1px solid grey;
+	border-top: none;
 
-  // Vertical Flex for section below image
-  display: flex;
-  flex-direction:row
+	// Vertical Flex for section below image
+	display: flex;
+	flex-direction: row;
 `;
 
 const TrackDetails = styled.div`
@@ -49,53 +49,57 @@ const TrackDetails = styled.div`
 	z-index: -1;
 	border-radius: 0 0 0 ${borderRadius};
 
-
 	// Padding for Text
- 	 padding: 2px 16px;
+	padding: 2px 16px;
 
- 	 // Vertical Flex Container for Text
- 	 display: flex;
- 	 flex-direction:column;
- 	 justify-content: space-around;
- 	 align-items: flex-start;
-
-`
-const SoundcloudButton = styled.div`
+	// Vertical Flex Container for Text
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: flex-start;
+`;
+const SoundcloudButton = styled.button`
 	flex: 25%;
 	background-color: orange;
 	object-fit: cover;
-	z-index: -1;
+	z-index: 1;
 	border-radius: 0 0 ${borderRadius} 0};
-`
+
+`;
 
 const StyledText = styled.span`
 	font-size: 12px;
 	margin: 0;
-`
+`;
 
-const icon = styled.i`
+const Icon = styled.i`
 	height:100%;
 	margin-top:auto;
 	margin-bottom:auto;
-`
+	&:hover {
+		color: white;
+		box-shadow:  0 8px 16px 0 rgba(0,0,0,0.2);,
+	}
+`;
 export const Card = props => {
-  const { imageSrc, trackTitle, trackArtist } = props;
+	const { imageSrc, trackTitle, trackArtist, trackURL } = props;
 
-  return (
-    <StyledCard>
-      <StyledCardImage src={imageSrc} />
-      <StyledCardDetails>
-      	<TrackDetails>
-      		<StyledText style={{order: 2}}> {trackTitle} </StyledText>
-      		<StyledText style={{order: 2}}> {trackArtist} </StyledText>
-      	</TrackDetails>
-      	<SoundcloudButton>
-      	 <span>
-              <icon className="fab fa-soundcloud"/>
-            </span>
-      	</SoundcloudButton>
-
-      </StyledCardDetails>
-    </StyledCard>
-  );
+	return (
+		<StyledCard>
+			<StyledCardImage src={imageSrc} />
+			<StyledCardDetails>
+				<TrackDetails>
+					<StyledText> {trackTitle} </StyledText>
+					<StyledText> {trackArtist} </StyledText>
+				</TrackDetails>
+					<SoundcloudButton
+						onClick={() => {
+							alert('link to track');
+						}}
+					>
+						<Icon className="fab fa-soundcloud" />
+					</SoundcloudButton>
+			</StyledCardDetails>
+		</StyledCard>
+	);
 };
