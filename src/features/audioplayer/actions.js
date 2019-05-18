3c9fd172c4c7fa68364b1874997300717625fa0c
sharad-s <sharadshekar@gmail.com>
2019-05-18 03:29:07 -0500

    Updated playlist

--- src/features/audioplayer/actions.js
+++ src/features/audioplayer/actions.js
@@ -14,7 +14,7 @@ import {
 import { setShortURL, getLongURL } from '../../utils/shorturl';
 
 const PLUG_PLAYLIST_URL =
-	'https://soundcloud.com/seanthebeatsmith/sets/freezing';
+	'https://soundcloud.com/seeyumour/sets/clios-simulacra';
 
 const baseURL = 'https://plug.af/';
 
