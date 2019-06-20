517cb4a729cd752708a0ebdd554cc60087485f9f
sharad-s <sharadshekar@gmail.com>
2019-06-19 22:11:34 -0500

    header update

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -5,10 +5,9 @@ import Sidebar from 'react-sidebar';
 // Redux
 import { connect } from 'react-redux';
 
-
 // Import Subcomponents
-import SidebarContent from "./SidebarContent"
-import MenuIcon from "../../images/menu.svg"
+import SidebarContent from './SidebarContent';
+import MenuIcon from '../../images/menu.svg';
 
 class LeftSidebar extends React.Component {
   constructor(props) {
@@ -18,15 +17,14 @@ class LeftSidebar extends React.Component {
       docked: false,
       open: false,
       transitions: true,
-      touch: true,  
+      touch: true,
       shadow: true,
       pullRight: false,
       touchHandleWidth: 20,
-      dragToggleDistance: 30
+      dragToggleDistance: 30,
     };
     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
     this.closeDrawer = this.closeDrawer.bind(this);
-
   }
 
   onSetSidebarOpen(open) {
@@ -34,7 +32,7 @@ class LeftSidebar extends React.Component {
   }
 
   closeDrawer() {
-    this.setState({sidebarOpen: false});
+    this.setState({ sidebarOpen: false });
   }
 
   // getAuthStatus(){
@@ -42,25 +40,30 @@ class LeftSidebar extends React.Component {
   //   let menuItems;
 
   //   if(auth.isAuthenticated) {
-  //     menuItems = 
+  //     menuItems =
   //   }
   // }
 
   render() {
-
     const { auth } = this.props;
+
+    // const renderedMenuButton = (
+    //   <button onClick={() => this.onSetSidebarOpen(true)}>
+    //     <img src={MenuIcon} className="header-item" id="MENU_BUTTON" />
+    //   </button>
+    // );
 
     return (
       <Sidebar
-        sidebar={<SidebarContent closeDrawer={this.closeDrawer} auth={auth}/>}
+        sidebar={<SidebarContent closeDrawer={this.closeDrawer} auth={auth} />}
         open={this.state.sidebarOpen}
         onSetOpen={this.onSetSidebarOpen}
         styles={{ sidebar: { background: 'black', width: '250px' } }}
       >
         <div className="header-container">
-          <button onClick={() => this.onSetSidebarOpen(true)}>
-            <img src={MenuIcon} className="header-item" id="MENU_BUTTON"/>
-          </button>  
+         <Link to="/create" className="header-item" id="MENU_BUTTON">
+            +create
+          </Link>
           <Link to="/" className="header-item" id="CENTER_ICON">
             plug
           </Link>
@@ -75,10 +78,7 @@ class LeftSidebar extends React.Component {
 
 const mapStateToProps = state => ({
   audio: state.audio,
-  auth: state.auth
+  auth: state.auth,
 });
 
-export default connect(
-  mapStateToProps,
-)(LeftSidebar); 
-
+export default connect(mapStateToProps)(LeftSidebar);
